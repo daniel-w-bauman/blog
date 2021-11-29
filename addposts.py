c55709de033fa03dfc44a37b8622cba0bc37adcd
daniel-w-bauman <daniel.w.bauman@gmail.com>
2021-11-29 00:57:11 -0500

    updated addresses to include blog/ because of github.io domain

--- addposts.py
+++ addposts.py
@@ -1,14 +1,14 @@
 from os import listdir
 blogname = 'Blog'
 lines = []
-fr = open('public/index.html', 'r')
+fr = open('docs/index.html', 'r')
 for l in fr.readlines():
 	lines.append(l)
 
-fw = open('public/index.html', 'w')
+fw = open('docs/index.html', 'w')
 for l in lines:
 	if 'id=\"post-item\"' not in l:
 		fw.write(l)
 		if 'id=\"posts\"' in l:
-			for f in listdir('public/posts'):
-				fw.write('\t\t\t\t<li id=\"post-item\" onclick=\"location.href = \'{}\';\">{}</li>\n'.format('/posts/{}'.format(f), ' '.join(f.split('-'))[:-5]))
+			for f in listdir('docs/posts'):
+				fw.write('\t\t\t\t<li id=\"post-item\" onclick=\"location.href = \'{}\';\">{}</li>\n'.format('/blog/posts/{}'.format(f), ' '.join(f.split('-'))[:-5]))
